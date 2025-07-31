6732f5c070f7a32c5046071a4dfd9f06f8be0837
unknown <yashpal2035@gmail.com>
2025-07-31 20:53:38 +0530

    my commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+DevOps Engineer – Junior/Associate Level
+
+Most common entry point into DevOps
+
+Work on CI/CD pipelines, basic scripting, Azure deployments
+
+Good for starting in both startups and MNCs
+
+Cloud Engineer – Azure (Entry-Level)
+
+Ideal due to your 4x Azure certifications
+
+Focuses on deploying and managing resources on Azure, sometimes with Terraform or Bicep
