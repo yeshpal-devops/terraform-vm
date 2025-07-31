11e1ee86c8ff8aaf2b1c1e87a2b9918d39828a0f
Yashpal2035 <yashpal2035@gmail.com>
2025-07-31 21:02:52 +0530

    terraform-code

--- main.tf
+++ main.tf
@@ -17,3 +17,30 @@ addcheck "
 # Terraform configuration for Azure VM deployment" {
   
 }
+
+resourcheck "name" {
+  type = "azurerm_virtual_machine"
+  name = "example_vm"
+  location = "East US"
+  resource_group_name = "example_resource_group"
+  network_interface_ids = ["${azurerm_network_interface.example.id}"]
+  vm_size = "Standard_DS1_v2"
+
+  storage_os_disk {
+    name              = "osdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "adminuser"
+    admin_password = "P@ssw0rd1234!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  } 
+  
+}
